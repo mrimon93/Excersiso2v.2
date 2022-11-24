a0f5ba7ecc49e57fcfe2d8a3388e3a1c43614f42
Muzammilkamaly <muzammil_kamaly93@live.se>
2022-11-24 13:53:48 +0100

    SQL-Excersise (Updatedv.2)

--- phonelist.py
+++ phonelist.py
@@ -1,29 +1,36 @@
 import psycopg2
 print("Welcome to the phone list, the following commands are available:")
 print("LIST, ADD, DELETE, QUIT")
-dbconn = psycopg2.connect(
-    host="localhost",
-    database="Phonelistdv",
-    user="postgres",
-    password="Monasogsql@12")
-def read_phonelist(conn):
+
+def SQL_Connection():
+    dbconn = psycopg2.connect(
+        host="localhost",
+        database="Phonelistdv",
+        user="postgres",
+        password="Monasogsql@12")
+    return dbconn
+def read_phonelist(conn,):
+    connection = SQL_Connection()
     cur = conn.cursor()
     cur.execute("SELECT * FROM phonelist;")
     rows = cur.fetchall()
     cur.close()
     return rows
 def add_phone(conn, name, phone):
+    connection = SQL_Connection()
     cur = conn.cursor()
     cur.execute(f"INSERT INTO phonelist VALUES ('{name}', '{phone}');")
     cur.execute("COMMIT;")
     cur.close()
     print(f"{name} added!")
 def delete_phone(conn, name):
+    connection = SQL_Connection()
     cur = conn.cursor()
     cur.execute(f"DELETE FROM phonelist WHERE name = '{name}';")
     cur.close()
     print(f"{name} deleted!")
 def save_phonelist(conn):
+    connection = SQL_Connection()
     cur = conn.cursor()
     try:
         cur.execute("COMMIT;")
@@ -40,11 +47,11 @@ while True: ## REPL - Read Execute Print Loop/Read Execute Program Loop
     elif cmd == "ADD":
         name = input("  Name: ")
         phone = input("  Phone: ")
-        add_phone(dbconn, name, phone)
+        add_phone(SQL_Connection(), name, phone)
     elif cmd == "DELETE":
         name = input("  Name: ")
-        delete_phone(dbconn, name)
+        delete_phone(SQL_Connection(), name)
     elif cmd == "QUIT":
-        save_phonelist(dbconn)
+        save_phonelist(SQL_Connection())
         print("Committing all changes to the database and quitting! Good bye!")
         exit()
